8ccb47a6f902d28b3a1776fbc05a0f5be6143a06
RiceGnat <ricegnat@gmail.com>
2020-10-21 22:30:37 -0500

    removed leftover line

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const port = (process.env.PORT || 8080);
 
-const tap = require("./router.js");
-
 express().use("/", express.static("build")).listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
